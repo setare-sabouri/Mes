347437d34ad39c25ddc00cb9ea9fefbdcc80df4e
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2026-07-19 04:34:32 +0330

    meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,12 @@ import Link from 'next/link';
 
 import styles from './page.module.css';
 import MealsGrid from '@/components/meals/meals-gird/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+
+    const meals = await getMeals();
+
     return (
         <>
             <header className={styles.header}>
@@ -21,7 +25,7 @@ export default function MealsPage() {
                 </p>
             </header>
             <main className={styles.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     );
